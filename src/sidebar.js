e96e19cdc5d7407af47370e4906ca5e18e49fced
NewGen2022 <amirgimari@gmail.com>
2024-04-07 21:39:07 +0300

    add tab for done tasks

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,6 +1,7 @@
 import inboxIcon from  './assets/all.png';
 import todayIcon from  './assets/availability.png';
 import thisWeekIcon from  './assets/bulletin-board.png';
+import doneIcon from "./assets/done.png";
 
 export default class Sidebar{
     constructor(){
@@ -12,14 +13,17 @@ export default class Sidebar{
         const inbox = document.createElement("button");
         const today = document.createElement("button");
         const thisWeek = document.createElement("button");
+        const done = document.createElement("button");
     
         const inboxImg = document.createElement("img");
         const todayImg = document.createElement("img");
         const thisWeekImg = document.createElement("img");
+        const doneImg = document.createElement("img");
     
         inboxImg.src = inboxIcon;
         todayImg.src = todayIcon;
         thisWeekImg.src = thisWeekIcon;
+        doneImg.src = doneIcon;
     
         inbox.appendChild(inboxImg);
         today.appendChild(todayImg);
@@ -28,14 +32,17 @@ export default class Sidebar{
         inbox.textContent = "Inbox";
         today.textContent = "Today";
         thisWeek.textContent = "This week";
+        done.textContent = "Done";
     
         inbox.classList.add("buttonDays", "with-icon");
         today.classList.add("buttonDays", "with-icon");
         thisWeek.classList.add("buttonDays", "with-icon");
+        done.classList.add("buttonDays", "with-icon");
     
         inbox.setAttribute("id", "inbox");
         today.setAttribute("id", "today");
         thisWeek.setAttribute("id", "thisWeek");
+        done.setAttribute("id", "done");
     
         const projectsHeader = document.createElement("h2");
         projectsHeader.textContent = "Projects";
@@ -49,6 +56,7 @@ export default class Sidebar{
         sidebar.appendChild(inbox);
         sidebar.appendChild(today);
         sidebar.appendChild(thisWeek);
+        sidebar.appendChild(done);
         sidebar.appendChild(projectsHeader);
         sidebar.appendChild(addProjectButton);
 
