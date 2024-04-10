15fdb05d80371f2cde271725d7b648e2f029f1ac
NewGen2022 <amirgimari@gmail.com>
2024-04-10 08:22:41 +0300

    added comments

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import generateHeader from "./header";
 import generateMain from "./main";
 import generateFooter from "./footer";
 
+// main file for generating all page
 generateHeader();
 generateMain();
 generateFooter();
